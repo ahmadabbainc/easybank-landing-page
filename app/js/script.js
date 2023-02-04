6d3ec3b1fea3ad2e9cab2440e5d66b9d2a338dab
ahmadabbainc <ahmedabbainc@gmail.com>
2023-02-04 01:30:36 +0100

    first commit

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,22 @@
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fadeElement = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener("click", function () {
   if (header.classList.contains("open")) {
     header.classList.remove("open");
-    overlay.classList.add("fade-out");
-    overlay.classList.remove("fade-in");
+
+    fadeElement.forEach((element) => {
+      element.classList.add("fade-out");
+      element.classList.remove("fade-in");
+    });
   } else {
     header.classList.add("open");
-    overlay.classList.add("fade-in");
-    overlay.classList.remove("fade-out");
+
+    fadeElement.forEach((element) => {
+      element.classList.add("fade-in");
+      element.classList.remove("fade-out");
+    });
   }
 });
